857aa3b7ad685ee160e9e4f991ebacca9ebbc59c
Mikhail Antonov <hermes@compot.ru>
2015-11-07 23:51:29 +0300

    Cleanup

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -16,13 +16,6 @@ namespace Snake
 				pnt.DrawChar();
 			}
 
-			List<int> numList = new List<int>();
-			numList.Add(77);
-			numList.Add(10);
-			numList.Add(34);
-
-			int cc = numList[1]; // 10
-
 			List<Point> pnList = new List<Point>();
 			pnList.Add(new Point(20, 20, 'x'));
 			pnList.Add(new Point(10, 15, 'y'));
